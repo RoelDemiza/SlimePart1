ff6a4e23e3e884d43a3a1e6b8358c59d82f416fc
Exocizcs <70126922+annieooijiayi@users.noreply.github.com>
2022-06-08 08:44:09 +0800

    Camera Shaking on Trigger enter
    - Implement 'CamTrigger' script to read Animator component from main camera
    
    - Record camera shaking animation and set parameter as 'SetBool'

---
+++ Assets/Scripts/CamTrigger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTrigger : MonoBehaviour
+{
+    [SerializeField] private new Camera camera;
+    private Animator anim;
+    private bool playerInRange;
+
+    private void Start()
+    {
+        anim = camera.GetComponent<Animator>();
+    }
+    private void Update() 
+    {
+        if (playerInRange)
+        {
+            anim.SetBool("isShaking", true);
+        }
+        else
+        {
+            anim.SetBool("isShaking", false);
+        }
+    }
+        
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        playerInRange = true;
+    }
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        playerInRange = false;
+    }
+}
